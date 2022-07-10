ab49c423b5fbf0d018ab83dd8fdaaf4d1698e814
vietsimon <vtran1555@gmail.com>
2022-07-10 22:05:23 +0700

    update package

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: "172.104.167.232",
+    port: 8202,
+    username: "postgres",
+    password: "Nintendo123",
+    database: "buff-vn-server",
     synchronize: true,
     logging: true,
     entities: [],
